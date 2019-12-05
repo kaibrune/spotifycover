fa56f244246aba71944737dc027cfdafb5102699
Kai Brune <info@kaibrune.com>
2019-12-05 23:28:56 +0100

    btn fix

--- 545f0b73aaa26538abb2209f5bf50000da953b85/js/script.js
+++ 545f0b73aaa26538abb2209f5bf50000da953b85/js/script.js
@@ -10,6 +10,7 @@ $(function () {
     $("#download").hide()
     $('#widget').hide();
     $("#btnSave").click(function () {
+        $("#download").hide()
         $("#img-out").html();
         var month = $('#month').val();
         $('#month_text').text(month);
