b4a5a05f066a3e0134d215a9244640a2e83986af
Kai Brune <info@kaibrune.com>
2020-06-19 15:49:33 +0200

    added split search

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 $(function () {
 
-    $('#month').keypress(function (e) {
+    $('#showsettings').click(function () {
+        $('#settings').show();
+        $('#standard').hide();
+    })
+
+    $('.input').keypress(function (e) {
         var key = e.which;
         if (key == 13) {
             $('#btnSave').click();
@@ -11,8 +16,9 @@ $(function () {
     $('#widget').hide();
     $("#btnSave").click(function () {
         $("#img-out").html();
-        var month = $('#month').val();
-        $('#month_text').text(month);
+        var text = $('#text').val();
+        var picture = $('#picture').val();
+        $('#text').text(text);
         $('#btnSave').html('<div class="loader">Loading<span class="loader__dot">.</span><span class="loader__dot">.</span><span class="loader__dot">.</span></div>');
 
         function toDataURL(src, callback, outputFormat) {
@@ -35,42 +41,10 @@ $(function () {
             }
         }
 
-        if (month == 'Januar') {
-            month = 'January'
-        };
-
-        if (month == 'Februar') {
-            month = 'February'
-        };
-
-        if (month == 'März') {
-            month = 'Spring'
-        };
-
-        if (month == 'Mai') {
-            month = 'May'
-        };
-
-        if (month == 'Juni') {
-            month = 'June'
-        };
-
-        if (month == 'Juli') {
-            month = 'July'
-        };
-
-        if (month == 'Oktober') {
-            month = 'October'
-        };
-
-        if (month == 'Dezember') {
-            month = 'December'
-        };
-
 
 
         toDataURL(
-            '//source.unsplash.com/500x500/?' + month,
+            '//source.unsplash.com/500x500/?' + picture,
             function (dataUrl) {
                 $('.bg').css("background-image", "url(" + dataUrl + ")");
             }
@@ -83,14 +57,9 @@ $(function () {
                     $("#img-out").html(canvas);
                     $("#download").show()
                     var a = document.getElementById("download");
-                    a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
-                    if (month == '') {
-                        month = 'unkown'
-                    };
-                    if (month == 'Spring') {
-                        month = 'märz'
-                    };
-                    a.download = month + '.jpg';
+                    a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream")
+
+                    a.download = text + '.jpg';
                 }
             });
             $('#widget').show();
