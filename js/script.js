71ba3c3e6c8be1352cae4c5c3ec9bab08affa19f
Kai Brune <info@kaibrune.com>
2020-06-19 23:46:44 +0200

    new design

--- js/script.js
+++ js/script.js
@@ -1,26 +1,56 @@
 $(function () {
+    $('#picture').hide();
+    $('#btnSave').hide();
+    $('#widget_con').hide();
+    var restart = false;
 
-    $('#showsettings').click(function () {
-        $('#settings').show();
-        $('#standard').hide();
-    })
-
-    $('.input').keypress(function (e) {
+    $('#text').keypress(function (e) {
         var key = e.which;
         if (key == 13) {
+
+
+            const element = document.querySelector('#text');
+            element.classList.add('animate__animated', 'animate__fadeOutDown');
+
+            element.addEventListener('animationend', () => {
+                $('#text').hide();
+                $('#picture').show();
+                $('#picture').addClass('animate__animated animate__fadeInDown');
+
+            });
+
+
+
+        }
+    });
+    $('#picture').keypress(function (e) {
+        var key = e.which;
+        if (key == 13) {
+
+            const element = document.querySelector('#picture');
+            element.classList.add('animate__animated', 'animate__fadeOutDown');
+
+            element.addEventListener('animationend', () => {
+                $('#picture').hide();
+                $('#btnSave').show();
+                $('#btnSave').addClass('animate__animated animate__fadeInDown');
+
+            });
+
+
             $('#btnSave').click();
             return false;
         }
     });
-    $("#download").hide()
-    $('#widget').hide();
+
     $("#btnSave").click(function () {
         $("#img-out").html();
         var text = $('#text').val();
         var picture = $('#picture').val();
-        if(text == ''){
+        if (text == '') {
             text = picture;
         }
+
         $('#month').text(text);
         $('#btnSave').html('<div class="loader">Loading<span class="loader__dot">.</span><span class="loader__dot">.</span><span class="loader__dot">.</span></div>');
 
@@ -58,21 +88,47 @@ $(function () {
                 onrendered: function (canvas) {
                     theCanvas = canvas;
                     $("#img-out").html(canvas);
-                    $("#download").show()
                     var a = document.getElementById("download");
                     a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream")
 
                     a.download = text + '.jpg';
                 }
             });
-            $('#widget').show();
+            const element = document.querySelector('#btnSave');
+            element.classList.add('animate__animated', 'animate__fadeOutDown');
+
+            element.addEventListener('animationend', () => {
+                $('#btnSave').hide();
+                $('#widget_con').show();
+                $('#widget_con').addClass('animate__animated animate__fadeInDown');
+
+            });
+
+
 
         }, 3500);
 
-        setTimeout(() => {
-            $('#widget').hide();
-            $('#btnSave').text('Generate cover');
-        }, 3600);
+        /*         setTimeout(() => {
+                    $('#picture').blur();
+                    restart = true;
+
+                    $(document).on('keypress',function(e) {
+                        if(e.which == 13 && restart == true) {
+                            restart = false;
+                            $('#widget_con').removeClass('animate__animated animate__fadeInDown');
+                            $('#btnSave').removeClass('animate__animated animate__fadeOutDown');
+                            const element = document.querySelector('#widget_con');
+                            element.classList.add('animate__animated', 'animate__fadeOutDown');
+                
+
+                
+                
+                            $('#btnSave').click();
+                            return false;
+                        }
+                    });
+                }, 3600);
+         */
 
     });
 });
