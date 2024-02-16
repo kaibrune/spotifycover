808aaaf80671b242ded79a9f5fa83107154d788c
kai <hej@kaibrune.com>
2024-02-16 10:47:15 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 $(function () {
     const animateCSS = (element, animation, prefix = 'animate__') =>
-        // We create a Promise and return it
         new Promise((resolve, reject) => {
             const animationName = `${prefix}${animation}`;
             const node = document.querySelector(element);
 
             node.classList.add(`${prefix}animated`, animationName);
 
-            // When the animation ends, we clean the classes and resolve the Promise
             function handleAnimationEnd() {
                 node.classList.remove(`${prefix}animated`, animationName);
                 node.removeEventListener('animationend', handleAnimationEnd);
